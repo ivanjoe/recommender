69e76d0ce48615e77df503b56de7bfe8ed7fced8
Ivan Kubinyi <ikubinyi@gmail.com>
2012-09-07 16:10:09 +0300

    Initial commit for the automatic recommender prototype

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AutomaticRecommender::Application.config.secret_token = '5659aff977ca711644d51c46c20f7fc320d828c01b1ea97cad39d29571ecdf0611924968fc11be38dad3dd75bc4927dd783d13a72ba1d385ed12591277bbea1c'
