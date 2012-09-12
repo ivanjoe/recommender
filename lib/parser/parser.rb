21e9090da834176249bf78512756c4d2646b0afc
Ivan Kubinyi <ikubinyi@gmail.com>
2012-09-12 15:16:20 +0300

    DB filled with ratings

--- lib/parser/parser.rb
+++ lib/parser/parser.rb
@@ -1,6 +1,13 @@
 #user id | item id | rating | timestamp
-lines = File.readlines('u.data')
+lines = File.readlines('/home/ivanjoe/Projects/automatic_recommender/lib/parser/u.data')
 ratings = lines.map do |line|
   line.strip.split(/\t/).map{|s| s.to_i}
 end
-p ratings.first
+ratings.each do |row|
+  r = Rating.new
+  r.user_id = row[0]
+  r.item_id = row[1]
+  r.rating = row[2]
+  r.rated_at = Time.at(row[3])
+  r.save!
+end
