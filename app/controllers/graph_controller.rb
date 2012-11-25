9eeae4d9c48917049101b433869a38e322e5ba18
Ivan Kubinyi <ikubinyi@gmail.com>
2012-11-25 20:08:46 +0200

    Counting the min and max for the graph is added

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -4,16 +4,57 @@ class GraphController < ApplicationController
 
     calculated_matrices = JSON.parse(json_matrices)
 
-    #head
-    head = [["X", "Row", "Column", "Actual user"]]
-    #rows
-    rows = calculated_matrices[1].map{|x| [x[0], x[1], nil, nil]}
-    #columns
-    columns = calculated_matrices[2].map{|y| [y[0],nil,y[1], nil] }
+    rows = []
+    x_min = nil
+    x_max = nil
+    y_min = nil
+    y_max = nil
 
-    #user
-    actual = [[0.6, nil, nil, 0.5]]
+    if params[:user]
+      #head
+      head = [["X", "Row", "Column", "Actual user"]]
+      #rows
+      #rows = calculated_matrices[1].map{|x| [x[0], x[1], nil, nil]}
+      calculated_matrices[1].each do |x|
+        rows.push([x[0], x[1], nil, nil])
+        
+        if x_min == nil or x_min > x[0]
+          x_min = x[0]
+        end
 
-    @json = (head + rows + columns+actual)   
+        if y_min == nil or y_min > x[1]
+          y_min = x[1]
+        end
+
+        if x_max == nil or x_max < x[0]
+          x_max = x[0]
+        end
+
+        if y_max == nil or y_max < x[1]
+          y_max = x[1]
+        end
+      end
+      
+      #columns
+      columns = calculated_matrices[2].map{|y| [y[0],nil,y[1], nil] }
+      # redo the same for this matrix as well
+
+      #user
+      actual = [[0.333, nil, nil, 0.5]]
+
+      @json = (head + rows + columns + actual)
+      @x_min = x_min
+      @x_max = x_max
+      @y_min = y_min
+      @y_max = y_max
+    else
+      #head
+      head = [["X", "Row", "Column"]]
+      #rows
+      rows = calculated_matrices[1].map{|x| [x[0], x[1], nil]}
+      #columns
+      columns = calculated_matrices[2].map{|y| [y[0],nil,y[1]] }
+      @json = (head + rows + columns)
+    end
   end
 end
