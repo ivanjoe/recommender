988049970919042e3ce57b3487e6a0a23a4942e8
Ivan Kubinyi <ikubinyi@gmail.com>
2013-01-12 20:32:03 +0200

    Ratings are shown after submission

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,10 @@ class HomeController < ApplicationController
     require Rails.root.to_s + '/lib/matrix_builder/items_array.rb'
 
     @movies = Array.new
+    @ratings = Hash.new
     Items_array.each_with_index do |item, index|
-      @movies.push(Movie.find_by_movie_id(item))
+      movie = Movie.find_by_movie_id(item)
+      @movies.push(movie)
       break if index == 20
     end
   end
