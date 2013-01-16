cdd019138910f1835d5bf7c3ea4c158652adb115
Ivan Kubinyi <ikubinyi@gmail.com>
2013-01-17 00:10:30 +0200

    Fix: hash names are only refered with strings

--- lib/parser/ratings_parser.rb
+++ lib/parser/ratings_parser.rb
@@ -3,10 +3,10 @@ all_ratings = JSON.parse(json)
 
 all_ratings.each do |rating|
   r = Rating.new
-  r.user_id = rating.user_id
-  r.item_id = rating.item_id
-  r.rating = rating.rating
-  r.rated_at = rating.rated_at
+  r.user_id = rating["user_id"]
+  r.item_id = rating["item_id"]
+  r.rating = rating["rating"]
+  r.rated_at = rating["rated_at"]
   r.save!
 end
 #heroku run 'rails runner lib/parser/movie_parser.rb'
